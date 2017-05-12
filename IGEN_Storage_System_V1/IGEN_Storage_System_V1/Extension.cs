08997fd3805dd19d6a3ab9cf9cd4ac2a300ab057
fholbech <holbech@outlook.com>
2017-05-12 12:19:00 +0200

    Update Extension.cs

--- IGEN_Storage_System_V1/IGEN_Storage_System_V1/Extension.cs
+++ IGEN_Storage_System_V1/IGEN_Storage_System_V1/Extension.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,32 @@ namespace IGEN_Storage_System_V1
 {
     class Extension
     {
+        public void Print1() //TODO - Find better name
+        {
+        }
+
+        public void Print2() //TODO - Find better name
+        {
+        }
+
+        public void Print3() //TODO - Find better name
+        {
+        }
+
+        public void CheckRights()
+        {
+        }
+
+        public void CleanGiverRegistry()
+        {
+        }
+
+        public void CleanItemRegistry()
+        {
+        }
+
+        public void CleanItemActionLog()
+        {
+        }
     }
 }
