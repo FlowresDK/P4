e13c1198396f289efd150b765dacda98efd6212a
rasmushyldahl <rasmushyldahl@hotmail.com>
2017-05-12 12:13:15 +0200

    Update User.cs

--- IGEN_Storage_System_V1/IGEN_Storage_System_V1/User.cs
+++ IGEN_Storage_System_V1/IGEN_Storage_System_V1/User.cs
@@ -8,5 +8,36 @@ namespace IGEN_Storage_System_V1
 {
     class User
     {
+        public int UserID {get; set;} 
+        public string FirstName {get; set;} 
+        public string LastName {get; set;} 
+        public int PhoneNumber {get; set}
+        public string Mail {get; set}
+        public string SteetName {get; set;} 
+        public string CityName {get; set;} 
+        public int ZipCode{get; set;} 
+        public int Role {get;set;}
+        public int Rights {get;set;}
+
+
+        public void CreateUser()
+        {
+        }
+
+        public void DeleteUser()
+        {
+        }
+
+        public void SearchUser()
+        {
+        }
+ 
+        public void EditUser()
+        {
+        }
+
+        public void ListUser()
+        {
+        }
     }
 }
