c7d109277605d26ee4f34225911f02b00bc65047
rasmushyldahl <rasmushyldahl@hotmail.com>
2017-05-12 12:07:40 +0200

    Update Item.cs

--- IGEN_Storage_System_V1/IGEN_Storage_System_V1/Item.cs
+++ IGEN_Storage_System_V1/IGEN_Storage_System_V1/Item.cs
@@ -8,7 +8,7 @@ namespace IGEN_Storage_System_V1
 {
     class Item
     {
-        public int ID { get; set; }
+        public int ItemID { get; set; }
         public int Category { get; set; }
         public int Price { get; set; }
         public string Comment { get; set; }
