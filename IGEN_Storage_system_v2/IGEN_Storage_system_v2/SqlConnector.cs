89d2951d8349b6e099d36996cc4d91a82eb56f32
rasmushyldahl <rasmushyldahl@hotmail.com>
2017-05-18 15:41:18 +0200

    Update SqlConnector.cs

--- IGEN_Storage_system_v2/IGEN_Storage_system_v2/SqlConnector.cs
+++ IGEN_Storage_system_v2/IGEN_Storage_system_v2/SqlConnector.cs
@@ -35,10 +35,9 @@ namespace IGEN_Storage_system_v2
         
         public static List<User> RetrieveUsers(string query)
 		{
-			SqlCommand cmd = new SqlCommand();
+		 	SqlCommand cmd = new SqlCommand();
 			SqlDataReader reader;
 			User tempUser = null;
-            List<User> userList = new List<User>();
 
 			cmd.CommandType = CommandType.Text;
 			cmd.CommandText = query;
@@ -48,41 +47,22 @@ namespace IGEN_Storage_system_v2
 
 			while (reader.Read())
 			{
-                //
-                // Henter data fra DB og laver et object
-                //
+               
 				int userID = Convert.ToInt32(reader["userID"].ToString());
-                string firstName = reader["firstname"].ToString();
-                string lastName = reader["lastname"].ToString();
-                int phoneNumber = Convert.ToInt32(reader["phonenumber"].ToString());
-                string mail = reader["mail"].ToString();
-                string streetName = reader["streetname"].ToString();
-                string cityName = reader["cityname"].ToString();
-                int zipCode = Convert.ToInt32(reader["zipcode"].ToString()); 
-                int role = Convert.ToInt32(reader["role"].ToString()); 
-                int rights = Convert.ToInt32(reader["rights"].ToString());
-                tempUser = new User(userID, firstName, lastName, phoneNumber, mail, streetName, cityName, zipCode, role, rights);
-                userList.Add(tempUser);
-
-                //
-                // Tar' data fra object og viser det som "placeholder"
-                //Dette kode skal nok være på panelet istedet på en eller anden måde.
-                //
-                TextboxNavnHer.Text = tempUser.UserID;
-                TextboxNavnHer.Text = tempUser.FirstName;
-                TextboxNavnHer.Text = tempUser.LastName;
-                TextboxNavnHer.Text = tempUser.PhoneNumber;
-                TextboxNavnHer.Text = tempUser.Mail;
-                TextboxNavnHer.Text = tempUser.StreetName;
-                TextboxNavnHer.Text = tempUser.CityName;
-                TextboxNavnHer.Text = tempUser.ZipCode;
-                TextboxNavnHer.Text = tempUser.Role;//her vil der bare blive vist et tal, så laves lidt lækkert der viser rolen bag tallet
-                TextboxNavnHer.Text = tempUser.Rights;//same as above
-
+                		string firstName = reader["firstname"].ToString();
+                		string lastName = reader["lastname"].ToString();
+                		int phoneNumber = Convert.ToInt32(reader["phonenumber"].ToString());
+                		string mail = reader["mail"].ToString();
+                		string streetName = reader["streetname"].ToString();
+                		string cityName = reader["cityname"].ToString();
+                		int zipCode = Convert.ToInt32(reader["zipcode"].ToString()); 
+                		int role = Convert.ToInt32(reader["role"].ToString()); 
+                		int rights = Convert.ToInt32(reader["rights"].ToString());
+                		tempUser = new User(userID, firstName, lastName, phoneNumber, mail, streetName, cityName, zipCode, role, rights);
+                		
 			}
 			sqlConn.Close();
 
-
-            return userList;
+            	return userList;
 		}
 }
