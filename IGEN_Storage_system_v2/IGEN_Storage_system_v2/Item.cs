4e7eaa150e5f647d6dcdf2b71530f0bb33a1e17c
rasmushyldahl <rasmushyldahl@hotmail.com>
2017-05-18 22:21:10 +0200

    Update Item.cs

--- IGEN_Storage_system_v2/IGEN_Storage_system_v2/Item.cs
+++ IGEN_Storage_system_v2/IGEN_Storage_system_v2/Item.cs
@@ -14,8 +14,9 @@ namespace IGEN_Storage_system_v2
         public string Comment { get; set; }
         private int NumberOfPrints;
 
-        public List<Item> SelectedItems{ get; set; } // List to be used with SelectItem-methods
-        public List<Item> ListOfAllItems;//Just a test list, DO NOT retrieve all items from database. 
+        List<Item> SelectedItemsList = new List<Item>(); // List to be used with SelectItem-methods
+        BindingSource bs = new BindingSource();
+       
 
         public void RegisterItem()
         {
@@ -24,14 +25,12 @@ namespace IGEN_Storage_system_v2
 
         public void DeleteItem(int userID)
         {
-            foreach (var item in SelectedItems)
-
+            foreach (var item in SelectedItemsList)
             {
                 SqlConnector.ExecuteSQL("DELETE FROM item WHERE itemID = " + item.ItemID);//Cheeck query-string upon implementation
                 SqlConnector.ExecuteSQL("INSERT INTO itemAction(Actiontype, State, UserID) VALUES(Item moved, Item discarded" + "," + userID);//Check query-string upon implementation
             }
-
-            SelectedItems = null;//clears the list, so that it is ready for next usages.
+            SelectedItemsList = null;//clears the list, so that it is ready for next usages.
         }
 
         public void SellItem()
@@ -39,42 +38,64 @@ namespace IGEN_Storage_system_v2
 
         }
 
+        
         public void MoveItem(int locationID, int userID)
-
         {
-            foreach (var item in SelectedItems)
-
+            foreach (var item in SelectedItemsList)
             {
                 SqlConnector.ExecuteSQL("UPDATE item SET location = " + locationID + "WHERE itemID = " + item.ItemID);//Check query-string upon implementation
                 SqlConnector.ExecuteSQL("INSERT INTO itemAction(Actiontype, State, UserID) VALUES(Item moved," + locationID + "," + userID);//Check query-string upon implementation
             }
 
-            SelectedItems = null;//clears the list, so that it is ready for next usages.
+            SelectedItemsList = null;//clears the list, so that it is ready for next usages.
         }
 
         //
-
         //Method for adding/removing item to list with scanner - to be used when moving/discarding/selling items.
-
         //
 
-        public void SelectItem(int choosenItemID)
-
+        public List<Item> CheckSelectedItem(Item tempItem, List<Item> selectedItems)
         {
-            var itemToChoose = ListOfAllItems[ListOfAllItems.FindIndex(item => item.ItemID == choosenItemID)];//some SQL her instead
-            //perhaps var itemToChoose = SqlConn(SELECT * FROM item WHERE itemID = choosenItemID)
+            int itemID = tempItem.ItemID;
 
-            if (!SelectedItems.Exists(item => item.ItemID == choosenItemID))
-
+            if (!selectedItems.Exists(item => item.ItemID == itemID))
             {
-                //SQL query here SELECT ..... itemId
-                SelectedItems.Add(itemToChoose);
+                selectedItems.Add(tempItem);
             }
-
             else
             {
-                SelectedItems.RemoveAll(item => item.ItemID == choosenItemID);
+                selectedItems.RemoveAll(item => item.ItemID == itemID);
             }
+            return selectedItems;
+        }
+        
+        //
+        //Når der klikkes på move item-button kaldes først ShowDataGrid - der viser et tomt datagrid. Når der så klikkes på vælges/scannes_button et item kaldes ItemSelector
+        //
+        public void ItemSelector(object sender, EventArgs e)
+        {
+            var enteredItemID = 35; //det her skal være input fra scanner/keyboard
+
+            var selectedItem = SqlConnector.RetrieveItem("SELECT * FROM item WHERE itemID =" + enteredItemID);
+
+            SelectedItemsList = CheckSelectedItem(selectedItem, SelectedItemsList);
+
+            ShowDataGridItems();
+        }
+        
+         public void ShowDataGridItems()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+            bs.DataSource = typeof(Item);
+
+            foreach(var item in SelectedItemsList)
+            {
+                bs.Add(item);
+            }
+
+            dataGridView1.DataSource = bs;
+            dataGridView1.AutoGenerateColumns = true;
         }
     }
 }
